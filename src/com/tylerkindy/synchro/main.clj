548f376232f8f948c9cb2279cbd034f6d143daf8
Tyler Kindy <tylerkindy@gmail.com>
2022-08-02 22:54:12 -0400

    Return 404 for non-HTML

--- src/com/tylerkindy/synchro/main.clj
+++ src/com/tylerkindy/synchro/main.clj
@@ -1,24 +1,27 @@
 (ns com.tylerkindy.synchro.main
   [:require
    [ring.adapter.jetty :refer [run-jetty]]
-   [hiccup.core :refer [html]]])
+   [hiccup.core :refer [html]]
+   [clojure.string :as str]])
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (html [:html
-                [:body
-                 [:h1 "Hello, World!"]]])})
+  (let [accept (get-in request [:headers "accept"])]
+    (if (str/includes? accept "text/html")
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       :body (html [:html
+                    [:body
+                     [:h1 "Hello, World!"]]])}
+      {:status 404})))
 
 (def server (atom nil))
 
 (defn restart!
   ([] (restart! false))
   ([join?]
-   (let [s @server]
-     (when s (.stop s))
-     (reset! server (run-jetty handler {:port 3000
-                                        :join? join?})))))
+   (when @server (.stop @server))
+   (reset! server (run-jetty handler {:port 3000
+                                      :join? join?}))))
 
 (defn -main []
   (restart! true))

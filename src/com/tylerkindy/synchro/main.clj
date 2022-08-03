4eed820d89c49ac06ed9b3a433811209ba3ad74b
Tyler Kindy <tylerkindy@gmail.com>
2022-08-02 22:26:31 -0400

    Set up Ring server

--- src/com/tylerkindy/synchro/main.clj
+++ src/com/tylerkindy/synchro/main.clj
@@ -1,4 +1,18 @@
-(ns com.tylerkindy.synchro.main)
+(ns com.tylerkindy.synchro.main
+  [:require [ring.adapter.jetty :refer [run-jetty]]])
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello, World!"})
+
+(def server (atom nil))
+
+(defn restart! [join?]
+  (let [s @server]
+    (when s (.stop s))
+    (reset! server (run-jetty handler {:port 3000
+                                       :join? join?}))))
 
 (defn -main []
-  (println "Hello, world!"))
+  (restart! true))

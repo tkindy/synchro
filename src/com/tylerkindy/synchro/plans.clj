383d82e4747738b4054295511511eb44f2c31a53
Tyler Kindy <tylerkindy@gmail.com>
2022-08-07 20:13:53 -0400

    Extract found plan page function

--- src/com/tylerkindy/synchro/plans.clj
+++ src/com/tylerkindy/synchro/plans.clj
@@ -10,12 +10,16 @@
     {:status 303
      :headers {"Location" (str "/plans/" id)}}))
 
+(defn found-plan-page [{:keys [description creator-name]}]
+  [:html
+   [:body
+    [:p (str description "," creator-name)]]])
+
 (defn plan-page [id]
   (let [plan (@plans id)
         response (if plan
-                   (let [{:keys [description creator-name]} plan]
-                     {:status 200
-                      :body (html [:html [:body [:p (str description "," creator-name)]]])})
+                   {:status 200
+                    :body (html (found-plan-page plan))}
                    {:status 404
                     :body (html [:html [:body [:p "Unknown plan"]]])})]
     (assoc response :headers {"Content-Type" "text/html"})))

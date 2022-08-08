8f203e0188fc32b466ad41f419e9b16b1de83b86
Tyler Kindy <tylerkindy@gmail.com>
2022-08-07 21:57:40 -0400

    Remove homepage CSS
    I have to rethink the grid formatting now that the dates are in play.

--- src/com/tylerkindy/synchro/css.clj
+++ src/com/tylerkindy/synchro/css.clj
@@ -3,13 +3,4 @@
    [garden.core :refer [css]]])
 
 (def main-css
-  (css [:body {:margin "auto"
-               :max-width "375px"}]
-       [:h1 :h2 {:text-align :center}]
-       [".new-plan-form" {:display :grid
-                          :grid-template-rows "25px 25px 25px"
-                          :grid-template-columns "auto auto"
-                          :gap "10px"
-                          :align-items :baseline}
-        [:label {:text-align :end}]
-        [:button {:grid-column "1 / span 2"}]]))
+  (css [:h1 :h2 {:text-align :center}]))

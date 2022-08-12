7f5f0a630f71d7de6df5a253a209532947efcdbe
Tyler Kindy <tylerkindy@gmail.com>
2022-08-12 09:27:46 -0400

    Add CSS rules for checkbox background images

--- src/com/tylerkindy/synchro/css.clj
+++ src/com/tylerkindy/synchro/css.clj
@@ -5,6 +5,16 @@
 (def home-css
   (css [:h1 :h2 {:text-align :center}]))
 
+(def checkbox-state-rules
+  (letfn [(url [state modifier]
+            (str "url(/public/" state "-" modifier ".svg)"))]
+    (->> ["checked" "unchecked" "ifneedbe"]
+         (map (fn [state]
+                [(str "&." state)
+                 [:&.active {:background-image (url state "active")}
+                  [:&:hover {:background-image (url state "hover")}]]
+                 [:&.inactive {:background-image (url state "inactive")}]])))))
+
 (def plan-css
   (css [:td.date-checkbox-cell {:padding "0 20px"}
         [:&.available {:background-color "green"}]]
@@ -13,4 +23,10 @@
                            :color "white"}]
         [:div {:padding "1px 0"}]
         [".day-of-month" {:font-size 18}]]
-       [".new-person-name" {:width "125px"}]))
+       [".new-person-name" {:width "125px"}]
+       (into []
+             (concat
+              [".checkbox" {:width "14px" :height "14px"
+                            :background-position "center"
+                            :background-size "contain"}]
+              checkbox-state-rules))))

beaa09133b831226440c24689f8261e9caff0d64
Tyler Kindy <tylerkindy@gmail.com>
2022-08-12 08:56:43 -0400

    Handle missing PORT env var

--- src/com/tylerkindy/synchro/main.clj
+++ src/com/tylerkindy/synchro/main.clj
@@ -2,16 +2,23 @@
   [:require
    [com.tylerkindy.synchro.routes :refer [app]]
    [ring.adapter.jetty :refer [run-jetty]]
-   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]])
+   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [clojure.string :as str]])
 
 (defonce server (atom nil))
+
+(defn env-port []
+  (let [env-var (System/getenv "PORT")]
+    (if (str/blank? env-var)
+      nil
+      (Integer/parseInt env-var))))
 
 (defn restart!
   ([] (restart! false))
   ([join?]
    (when @server (.stop @server))
    (reset! server (run-jetty (wrap-defaults app site-defaults)
-                             {:port (or (Integer/parseInt (System/getenv "PORT")) 3000)
+                             {:port (or (env-port) 3000)
                               :join? join?}))))
 
 (defn -main []

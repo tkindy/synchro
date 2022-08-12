8dd669f95461155b230902af5e4819ee4d6bb8ca
Tyler Kindy <tylerkindy@gmail.com>
2022-08-12 09:52:22 -0400

    Split out checkbox URL map

--- src/com/tylerkindy/synchro/css.clj
+++ src/com/tylerkindy/synchro/css.clj
@@ -5,15 +5,22 @@
 (def home-css
   (css [:h1 :h2 {:text-align :center}]))
 
+(def checkbox-urls
+  (letfn [(url-entry [state modifier]
+            [modifier (str "/public/" (name state) "-" (name modifier) ".svg")])]
+    (->> [:checked :unchecked :ifneedbe]
+         (map (fn [state] {state (into {} (list (url-entry state :active)
+                                                (url-entry state :hover)
+                                                (url-entry state :inactive)))}))
+         (apply merge))))
+
 (def checkbox-state-rules
-  (letfn [(url [state modifier]
-            (str "url(/public/" state "-" modifier ".svg)"))]
-    (->> ["checked" "unchecked" "ifneedbe"]
-         (map (fn [state]
-                [(str "&." state)
-                 [:&.active {:background-image (url state "active")}
-                  [:&:hover {:background-image (url state "hover")}]]
-                 [:&.inactive {:background-image (url state "inactive")}]])))))
+  (->> [:checked :unchecked :ifneedbe]
+       (map (fn [state]
+              [(str "&." (name state))
+               [:&.active {:background-image (get-in checkbox-urls [state :active])}
+                [:&:hover {:background-image (get-in checkbox-urls [state :hover])}]]
+               [:&.inactive {:background-image (get-in checkbox-urls [state :inactive])}]]))))
 
 (def plan-css
   (css [:td.date-checkbox-cell {:padding "0 20px"}

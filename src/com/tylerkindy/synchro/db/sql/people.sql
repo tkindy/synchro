df40995f9ef452fff2ee14ab95fa1739ba3b4381
Tyler Kindy <tylerkindy@gmail.com>
2022-08-13 00:00:40 -0400

    More queries

--- src/com/tylerkindy/synchro/db/sql/people.sql
+++ src/com/tylerkindy/synchro/db/sql/people.sql
@@ -6,3 +6,14 @@ RETURNING id;
 -- :name insert-person-dates :! :n
 INSERT INTO people_dates (person_id, date, state)
 VALUES :tuple*:people-dates;
+
+-- :name get-people :? :*
+SELECT id, name
+FROM people
+WHERE plan_id = :plan-id;
+
+-- :name get-people-dates :? :*
+SELECT pd.person_id, pd.date, pd.state
+FROM people_dates pd
+JOIN people p ON pd.person_id = p.id
+WHERE p.plan_id = :plan-id;
--- src/com/tylerkindy/synchro/db/sql/plans.sql
+++ src/com/tylerkindy/synchro/db/sql/plans.sql
@@ -1,7 +1,15 @@
 -- :name insert-plan :! :n
-INSERT INTO plans (description)
-VALUES (:description);
+INSERT INTO plans (id, description)
+VALUES (:id, :description);
 
 -- :name insert-dates :! :n
 INSERT INTO plan_dates (plan_id, date)
 VALUES :tuple*:dates;
+
+-- :name get-plan :? :1
+SELECT * FROM plans
+WHERE id = :id;
+
+-- :name get-plan-dates :? :*
+SELECT date FROM plan_dates
+WHERE plan_id = :id;

89ce30a51f4a3afb81af0a34d766fa55a6e21169
Tyler Kindy <tylerkindy@gmail.com>
2022-08-17 22:30:46 -0400

    Use HikariCP

--- src/com/tylerkindy/synchro/db/core.clj
+++ src/com/tylerkindy/synchro/db/core.clj
@@ -1,5 +1,5 @@
 (ns com.tylerkindy.synchro.db.core
-  (:require [next.jdbc :as jdbc]
+  (:require [hikari-cp.core :refer [make-datasource]]
             [next.jdbc.result-set :refer [as-unqualified-kebab-maps]]
             [hugsql.core :as hugsql]
             [hugsql.adapter.next-jdbc :refer [hugsql-adapter-next-jdbc]]))
@@ -7,4 +7,4 @@
 (hugsql/set-adapter! (hugsql-adapter-next-jdbc {:builder-fn as-unqualified-kebab-maps}))
 (def db (or (System/getenv "JDBC_DATABASE_URL")
             "jdbc:postgresql:postgres?user=postgres&password=password"))
-(def ds (jdbc/get-datasource db))
+(def ds (make-datasource {:jdbc-url db}))

9389583f65abe686f1e65df6eca3443192b31c70
Tyler Kindy <tylerkindy@gmail.com>
2022-08-25 08:26:04 -0400

    Use config for database URL in app

--- src/com/tylerkindy/synchro/db/core.clj
+++ src/com/tylerkindy/synchro/db/core.clj
@@ -2,9 +2,8 @@
   (:require [hikari-cp.core :refer [make-datasource]]
             [next.jdbc.result-set :refer [as-unqualified-kebab-maps]]
             [hugsql.core :as hugsql]
-            [hugsql.adapter.next-jdbc :refer [hugsql-adapter-next-jdbc]]))
+            [hugsql.adapter.next-jdbc :refer [hugsql-adapter-next-jdbc]]
+            [com.tylerkindy.synchro.config :refer [config]]))
 
 (hugsql/set-adapter! (hugsql-adapter-next-jdbc {:builder-fn as-unqualified-kebab-maps}))
-(def db (or (System/getenv "JDBC_DATABASE_URL")
-            "jdbc:postgresql:postgres?user=postgres&password=password"))
-(def ds (make-datasource {:jdbc-url db}))
+(def ds (make-datasource {:jdbc-url (:jdbcUrl config)}))

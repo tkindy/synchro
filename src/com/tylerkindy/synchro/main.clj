3b082ae743a4c6c792261fd9f0a93b22b4f3078c
Tyler Kindy <tylerkindy@gmail.com>
2022-08-25 08:57:52 -0400

    Put port in config

--- src/com/tylerkindy/synchro/main.clj
+++ src/com/tylerkindy/synchro/main.clj
@@ -3,22 +3,16 @@
    [com.tylerkindy.synchro.routes :refer [app]]
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-   [clojure.string :as str]])
+   [com.tylerkindy.synchro.config :refer [config]]])
 
 (defonce server (atom nil))
-
-(defn env-port []
-  (let [env-var (System/getenv "PORT")]
-    (if (str/blank? env-var)
-      nil
-      (Integer/parseInt env-var))))
 
 (defn restart!
   ([] (restart! false))
   ([join?]
    (when @server (.stop @server))
    (reset! server (run-jetty (wrap-defaults app site-defaults)
-                             {:port (or (env-port) 3000)
+                             {:port (:port config)
                               :join? join?}))))
 
 (comment (restart!))

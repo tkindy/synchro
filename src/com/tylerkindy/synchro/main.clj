de7ac2b6931e685114fdf7c836305c7c6a7f46b6
Tyler Kindy <tylerkindy@gmail.com>
2022-08-25 22:15:04 -0400

    Use encrypted cookie sessions

--- src/com/tylerkindy/synchro/main.clj
+++ src/com/tylerkindy/synchro/main.clj
@@ -3,15 +3,30 @@
    [com.tylerkindy.synchro.routes :refer [app]]
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [ring.middleware.session.cookie :refer [cookie-store]]
    [com.tylerkindy.synchro.config :refer [config]]])
 
 (defonce server (atom nil))
+
+(defn parse-session-secret [secret]
+  (-> (java.util.HexFormat/of)
+      (.parseHex secret)))
+
+(def app-settings
+  (let [session-store (cookie-store {:key (parse-session-secret
+                                           (get-in config [:http :session-secret]))})]
+    (-> site-defaults
+        (assoc-in [:session :store] session-store)
+
+        ; need to use non-default name to avoid old session
+        ; cookies causing decryption failures
+        (assoc-in [:session :cookie-name] "ring-session2"))))
 
 (defn restart!
   ([] (restart! false))
   ([join?]
    (when @server (.stop @server))
-   (reset! server (run-jetty (wrap-defaults app site-defaults)
+   (reset! server (run-jetty (wrap-defaults app app-settings)
                              {:port (get-in config [:http :port])
                               :join? join?}))))
 

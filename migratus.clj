1961773f81339b79d3a0848d470fadfe782fbb53
Tyler Kindy <tylerkindy@gmail.com>
2022-10-04 18:39:06 -0400

    Mount config for use in migrations

--- migratus.clj
+++ migratus.clj
@@ -1,5 +1,12 @@
 (ns migratus
-  (:require [com.tylerkindy.synchro.config :refer [config]]))
+  (:require [com.tylerkindy.synchro.config :refer [config]]
+            [mount.core :as mount]))
+
+(-> (mount/only #{#'com.tylerkindy.synchro.config/file-config
+                  #'com.tylerkindy.synchro.config/cli-args
+                  #'com.tylerkindy.synchro.config/config})
+    (mount/swap {#'com.tylerkindy.synchro.config/cli-args {}})
+    mount/start)
 
 {:store :database
  :db (-> (:db config)

20a2fabfd028772ea1a3c27d5a044d9017980ca3
Tyler Kindy <tylerkindy@gmail.com>
2022-10-30 10:02:59 -0400

    Use more specific names and selector in home JS

--- resources/home.js
+++ resources/home.js
@@ -1,8 +1,8 @@
-const dateContainer = document.querySelector(".dates");
+const manualDateContainer = document.querySelector(".manual-dates .dates");
 
 document.getElementById("add-manual-dates").addEventListener("click", () => {
   const newDate = document.createElement("input");
   newDate.name = "date";
   newDate.type = "date";
-  dateContainer.appendChild(newDate);
+  manualDateContainer.appendChild(newDate);
 });

5ccc073b856f9580944e1c9a51b48c7473480bc5
Tyler Kindy <tylerkindy@gmail.com>
2022-10-30 10:31:37 -0400

    Add JS for changing active date input

--- resources/home.js
+++ resources/home.js
@@ -8,3 +8,20 @@ document.getElementById("add-manual-dates").addEventListener("click", () => {
   newDate.type = "date";
   manualDateContainer.appendChild(newDate);
 });
+
+const dateInputWrappers = document.querySelectorAll(".date-input-wrapper");
+
+document.querySelector(".date-input-select").addEventListener("change", (e) => {
+  const inputType = e.target.selectedOptions[0].value;
+
+  dateInputWrappers.forEach((wrapper) => {
+    const classes = new Set(wrapper.classList);
+    if (classes.has(inputType)) {
+      classes.add("active");
+    } else {
+      classes.delete("active");
+    }
+
+    wrapper.className = Array.from(classes).join(" ");
+  });
+});

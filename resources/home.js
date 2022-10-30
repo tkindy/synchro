1d90e23cd773a7d6e341567cd832c0438a3ec90a
Tyler Kindy <tylerkindy@gmail.com>
2022-10-30 11:02:24 -0400

    Simplify date input toggling
    It turns out `classList` is more like a set than a list in that `add`
    and `remove` are essentially set operations.

--- resources/home.js
+++ resources/home.js
@@ -15,13 +15,11 @@ document.querySelector(".date-input-select").addEventListener("change", (e) => {
   const inputType = e.target.selectedOptions[0].value;
 
   dateInputWrappers.forEach((wrapper) => {
-    const classes = new Set(wrapper.classList);
-    if (classes.has(inputType)) {
+    const classes = wrapper.classList;
+    if (classes.contains(inputType)) {
       classes.add("active");
     } else {
-      classes.delete("active");
+      classes.remove("active");
     }
-
-    wrapper.className = Array.from(classes).join(" ");
   });
 });
